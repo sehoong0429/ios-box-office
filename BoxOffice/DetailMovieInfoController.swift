f37d8a4ffa9b907b7cf202cf9552d95dc3da2f38
sehong <tjsgh0801@naver.com>
2023-03-29 08:59:55 +0900

    chore: detailViewcontroller 추가

---
+++ BoxOffice/DetailMovieInfoController.swift
@@ -0,0 +1,12 @@
+//
+//  DetailMovieInfoController.swift
+//  BoxOffice
+//
+//  Created by Seoyeon Hong on 2023/03/28.
+//
+
+import UIKit
+
+final class DetailMovieInfoController: UIViewController {
+    
+}
